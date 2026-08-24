2557a581edea9d36b0f2032fb0860bbca658bbe7
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-24 19:52:22 +0600

    test(repository): align table fixtures with test namespace

--- tests/Unit/TableRepositoryEvolutionTest.php
+++ tests/Unit/TableRepositoryEvolutionTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace Infocyph\DBLayer\Tests\Unit;
+
 use Infocyph\DBLayer\DB;
 use Infocyph\DBLayer\Exceptions\UnwritableAttributeException;
 use Infocyph\DBLayer\Query\QueryBuilder;
